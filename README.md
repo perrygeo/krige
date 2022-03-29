2fa6a5e301cad1e4c8021e69a938cbf57d7824d4
mperry <perrygeo@gmail.com>
2022-03-29 10:33:54 -0600

    kdtree for nn selection

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ```
+cargo build --release && time ./target/release/rust-interpolate --points ~/work/mosaic1/points.xyz --samples 5800 --range 0.2 --nbins 1200 > /dev/null
+
+
 cargo build --release && \
   time ./target/release/rust-interpolate --points test.xyz --samples 5 --range 10 --nbins 100 > /tmp/pairs.txt && \
 	gnuplot -p -e "plot '/tmp/pairs.txt' with points;"
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 // TODO
-// Build kdtree and find X observations nearest the unknown
 // fit model automaticaly
 //   https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html#scipy-optimize-curve-fit
 // support other models: Exponential, Linear, Gaussian
@@ -30,6 +29,8 @@ use std::fs::File;
 
 use clap::Parser;
 use itertools::Itertools;
+use kdtree::distance::squared_euclidean;
+use kdtree::KdTree;
 use nalgebra::{DMatrix, DVector};
 use rand::{seq::IteratorRandom, thread_rng};
 
@@ -44,16 +45,16 @@ struct Args {
     #[clap(short, long)]
     points: String,
 
-    /// Number of locations to sample
+    /// Maxiumum Range or Lag distance to consider
+    #[clap(short, long)]
+    range: f64,
+
+    /// Number of locations to sample for the empirical semivariogram
     #[clap(short, long, default_value_t = 5000)]
     samples: usize,
 
-    /// Maxiumum Range or Lag distance to consider
-    #[clap(short, long, default_value_t = 0.4)]
-    range: f64,
-
-    /// Number of lag bins
-    #[clap(short, long, default_value_t = 100)]
+    /// Number of lag bins for the empirical semivariogram
+    #[clap(short, long, default_value_t = 256)]
     nbins: usize,
 }
 
@@ -61,6 +62,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     // Parse XYZ Locations from input text
+    eprintln!("Parse XYZ file...");
     let file = File::open(args.points)?;
     let locs = parse_locations(file)?;
 
@@ -69,7 +71,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let samples = locs.iter().choose_multiple(&mut rng, args.samples);
 
     // -------- Create empirical semivariogram
-    eprintln!("creating empirical semivariogram...");
+    eprintln!("Empirical semivariogram...");
+
     // Create lag bin map, holding the sum and count (partials required to calculate the mean)
     let mut variogram_bins = Vec::with_capacity(args.nbins);
     for b in (0..args.nbins).into_iter() {
@@ -78,7 +81,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         variogram_bins.insert(b, LagBin::new(h));
     }
 
-    // Pass 1:
+    // subset Pass 1:
     // Test only unique pairs within the max range
     // and create the semivariogram
     for pair in samples.iter().combinations(2) {
@@ -92,10 +95,21 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let variance = calculate_variance(&si, &sj);
 
-        let lagbin_idx = (args.nbins as f64 * (dist / args.range)) as usize; // should always be 0 to args.nbins
+        // should always be 0 to args.nbins
+        let lagbin_idx = (args.nbins as f64 * (dist / args.range)) as usize;
+
         let lagbin = &mut variogram_bins[lagbin_idx];
         lagbin.push(variance);
     }
+
+    // -------- Create spatial index
+    eprint!("Creating spatial index...");
+    let mut kdtree = KdTree::new(2);
+    // Complete dataset pass 1
+    for loc in locs.iter() {
+        kdtree.add([loc.x, loc.y], loc)?;
+    }
+    eprintln!("built kdtree with {} pts", kdtree.size());
 
     // -------- Model specification
     // Create a model fit to the empirical variogram
@@ -110,22 +124,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     // let model = SphericalVariogramModel::new(2.5, 7.5, 10.0);
     // let pt = (5.0, 5.0);
 
-    // Identify pairs of data points
-    // Rule of thumb: 64 nearest points
-    // TODO Knn
-    //
-    // Note that these pairs do not need to be the same as what created the empirical variogram
-    // shadowing `samples` for convenience
-    // IOW now that we have a variogram model, we can use a much smaller number of
-    // samples in a local window.
-    let samples = locs.iter().choose_multiple(&mut rng, 956);
+    // Identify nearby data points
+    let neighbors = kdtree.nearest(&[pt.0, pt.1], 32, &squared_euclidean)?;
 
     // create matrix A
     eprintln!("Create and invert matrix A...");
-    let mut matrix_values = Vec::with_capacity((samples.len() + 1).pow(2));
-    for si in samples.iter() {
-        for sj in samples.iter() {
-            // is it more efficient to store the distance matrix in the first pass? defer
+    let num_neighbors = neighbors.len();
+    let mut matrix_values = Vec::with_capacity((num_neighbors + 1).pow(2));
+    for (_, si) in neighbors.iter() {
+        for (_, sj) in neighbors.iter() {
             let dist = distance(&si, &sj);
             let modeled_semivariance = model.estimate(dist);
             matrix_values.push(modeled_semivariance);
@@ -134,28 +141,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         matrix_values.push(1.0);
     }
     // Add a final row to ensure weights sum to one
-    for _ in (0..samples.len()).into_iter() {
+    for _ in (0..num_neighbors).into_iter() {
         matrix_values.push(1.0);
     }
     matrix_values.push(0.0);
 
     // instatiate matrix and take inverse
-    let a = DMatrix::from_vec(samples.len() + 1, samples.len() + 1, matrix_values);
-    // eprintln!("{}", a);
+    let a = DMatrix::from_vec(num_neighbors + 1, num_neighbors + 1, matrix_values);
     let a_inv = a.try_inverse().expect("inverting matrix failed");
-    // eprintln!("{}", a_inv);
-
-    eprintln!("Prediction; create vector b ...");
 
     // distances from unknown pt to observations
     // plug in distances to the variogram estimator to create vector b
-    let mut vector_values = Vec::with_capacity(samples.len() + 1);
+    eprintln!("Prediction; create vector b ...");
+    let mut vector_values = Vec::with_capacity(num_neighbors + 1);
     let unknown = Location {
         x: pt.0,
         y: pt.1,
         z: 0.0,
     };
-    for s in samples.iter() {
+    for (_, s) in neighbors.iter() {
         let dist = distance(&s, &unknown);
         let modeled_semivariance = model.estimate(dist);
         vector_values.push(modeled_semivariance);
@@ -164,21 +168,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     vector_values.push(1.0);
 
     let b = DVector::from_vec(vector_values);
-    let b2 = b.clone();
 
     // Do the matrix multiplication
-    let weights_vector = a_inv * b;
+    let weights_vector = a_inv * &b;
 
     // predict the unknown value at pt using sum(weight * observed_z)
     let mut predicted_value = 0.0;
-    for (i, s) in samples.iter().enumerate() {
+    for (i, (_, s)) in neighbors.iter().enumerate() {
         predicted_value += s.z * weights_vector[i];
     }
 
-    // estimate the kriging error (stddev or sqrt of variance) at pt using
+    // estimate the kriging variance at a point
+    // error in z units is the stddev (the sqrt of variance)
     // Should be possible to do in matrix methods: let estvar = (b2 * weights_vector).sum();
     let mut estimation_variance = 0.0;
-    for (i, bsv) in b2.iter().enumerate() {
+    for (i, bsv) in b.iter().enumerate() {
         estimation_variance += bsv * weights_vector[i];
     }
 
@@ -193,12 +197,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     for lagbin in variogram_bins.iter() {
         // Empirical Semivariogram
         println!("{} {}", lagbin.h, lagbin.mean() / 2.0,);
+        // // Compare empirical vs modelled semivariances
+        // println!("{} {}", lagbin.mean() / 2.0, model.estimate(lagbin.h));
     }
-
-    // for lagbin in variogram_bins.iter() {
-    //     // Compare empirical vs modelled semivariances
-    //     println!("{} {}", lagbin.mean() / 2.0, model.estimate(lagbin.h));
-    // }
 
     Ok(())
 }
