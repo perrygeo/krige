68b214584a59a10d215f50c2deb0d537a3cb73a4
mperry <perrygeo@gmail.com>
2022-03-30 10:08:09 -0600

    fix alignment, calculate extent and cellsize

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@
 use itertools::Itertools;
 use std::error::Error;
 use std::fs::File;
+// simd::f64x2};
 
 use clap::Parser;
 use kdtree::{distance::squared_euclidean, KdTree};
@@ -59,6 +60,20 @@ struct Args {
     max_neighbors: usize,
 }
 
+fn adjust_extent(extent: &mut [f64], x: f64, y: f64) {
+    if x < extent[0] {
+        extent[0] = x;
+    } else if x > extent[2] {
+        extent[2] = x;
+    }
+
+    if y < extent[1] {
+        extent[1] = y;
+    } else if y > extent[3] {
+        extent[3] = y;
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let mut rng = thread_rng();
@@ -72,6 +87,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     eprintln!("Create spatial index and removing dups...");
     const EPSILON: f64 = 1e-9 * 1e-9;
     let mut kdtree = KdTree::new(2);
+    // while we're at it, calculate the bounding box
+    let mut extent = [
+        f64::INFINITY,
+        f64::INFINITY,
+        f64::NEG_INFINITY,
+        f64::NEG_INFINITY,
+    ];
     for loc in locs.iter() {
         // If existing points, check to avoid dups
         // there must be a better way!
@@ -79,13 +101,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         if nns.len() > 0 {
             let (sqdist, _existing) = nns[0];
             if sqdist < EPSILON {
-                // Duplicate point exists, skipping
+                // Duplicate point exists, skip
                 continue;
             }
         }
         // New unique point
         kdtree.add([loc.x, loc.y], loc)?;
+        adjust_extent(&mut extent, loc.x, loc.y);
     }
+    eprintln!("extent {:?}", extent);
 
     // -------- Create empirical semivariogram from a sample
     eprintln!("Empirical semivariogram ...");
@@ -94,30 +118,35 @@ fn main() -> Result<(), Box<dyn Error>> {
     let model = fit_model(variogram_bins);
 
     // -------- Prediction
-    // TODO estimate these
-    let cellsize: f64 = 0.0005;
-    let extent = (-125.0, 41.5, -124.7, 42.0);
+    // estimate cellsize such that each row is roughly x columns wide to cover the extent
+    let cellsize: f64 = (extent[2] - extent[0]) / 128.;
 
     // Create a 2D raster grid
-    let ulorigin = (extent.0, extent.3);
-    let llorigin = (extent.0, extent.1);
-    let rows = ((extent.3 - extent.1) / cellsize).ceil() as usize;
-    let cols = ((extent.2 - extent.0) / cellsize).ceil() as usize;
-    eprintln!("Making predictions on grid... {} x {}", rows, cols);
+    let halfcell = cellsize / 2.0;
+    let ulorigin = (extent[0], extent[3]);
+    let rows = ((extent[3] - extent[1]) / cellsize).ceil() as usize;
+    let cols = ((extent[2] - extent[0]) / cellsize).ceil() as usize;
+    // alignment in QGIS seems to require the upper-left corner of the lower-left cell
+    // whereas the ascii grid spec uses the llcorner
+    let llcorner = (ulorigin.0, ulorigin.1 - (cellsize * rows as f64));
+
+    eprintln!(
+        "Making predictions on grid... {} x {} with cellsize {}",
+        cols, rows, cellsize
+    );
 
     println!("ncols {}", cols);
     println!("nrows {}", rows);
-    // TODO alignment; add halfcell?
-    println!("xllcorner {}", llorigin.0);
-    println!("yllcorner {}", llorigin.1);
+    println!("xllcorner {}", llcorner.0);
+    println!("yllcorner {}", llcorner.1);
     println!("cellsize {}", cellsize);
     println!("NODATA_value -9999");
 
     for i in 0..rows {
-        let y = ulorigin.1 - (i as f64 * cellsize);
+        let y = ulorigin.1 - (i as f64 * cellsize) - halfcell;
         let mut row = Vec::with_capacity(cols);
         for j in 0..cols {
-            let x = ulorigin.0 + (j as f64 * cellsize);
+            let x = ulorigin.0 + (j as f64 * cellsize) + halfcell;
             let pt = (x, y);
 
             // Identify nearby data points
